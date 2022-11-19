5ce1065b1210280cb0fc836e42e0f10678f59dba
eylonshm <eylonshm@gmail.com>
2022-11-19 22:38:03 +0200

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Example from './components/example.jsx'
+
+function App() {
+  return (
+    <div className='flex justify-center items-center w-screen	h-screen p-0'>
+      <Example />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/example.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Example = () => {
+  return (
+    <div className='flex items-center justify-center text-black-200 w-[350px] h-1/2 bg-yellow-100 rounded-2xl text-5xl font-medium'>
+      <span>Tailwind CSS</span>
+    </div>
+  )
+}
+export default Example
